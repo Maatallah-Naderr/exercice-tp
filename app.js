c9647f742c5b8629e2dbe4bee26c4e8c17746b81
Maatallah-Naderr <nadermaatallah8@gmail.com>
2025-01-06 21:23:25 +0100

    exercice

---
+++ app.js
@@ -0,0 +1,40 @@
+let massJohn = 92
+let heighJohn = 1.95
+let bmiJohn = massJohn/ (heighJohn**2)
+
+console.log( "john:",bmiJohn)
+
+
+let massMark=78
+let heighMark=1.69
+let bmiMark= massMark/(heighMark**2)
+console.log( "mark:",bmiMark)
+
+
+// let markHeigherBmi=bmiMark < bmiJohn
+// console.log(markHeigherBmi)
+
+
+//1
+
+//   if (bmiMark < bmiJohn)
+//  {
+//      console.log("jhons bmi is heihger then mark")
+
+//  }
+//  else 
+//  {
+//      console.log("mark bmi is heihger then jhon")
+//  }
+
+//2
+if (bmiMark < bmiJohn)
+    {
+        console.log(`jhons bmi(${bmiJohn}) is heihger then mark (${bmiMark}) `)
+   
+    }
+    else 
+    {
+        console.log(`mark bmi(${bmiMark}) is heihger then jhon (${bmiJohn})`)
+    }
+   
